f8c89f3cb780077e7aff4c0f3605e38b55cad3be
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2022-06-02 11:26:01 -0400

    update the timer tests

--- src/components/Timer.cy.js
+++ src/components/Timer.cy.js
@@ -22,23 +22,33 @@ describe('Timer', () => {
     cy.contains('.status__time', '00:03')
   })
 
-  it.skip('controls the clock', () => {
-    // cy.clock and cy.tick do not work with component testing yet
-    cy.clock().then(() => {
-      cy.mount(
-        <SudokuContext.Provider value={{ timeGameStarted: moment() }}>
-          <section className="status">
-            <Timer />
-          </section>
-        </SudokuContext.Provider>,
-      )
-    })
-    cy.contains('.status__time', '00:00').then(() => {
-      cy.tick(30_000)
-      cy.tick(30_000)
-    })
-    // cy.tick(1000)
-    // cy.tick(1000)
-    // cy.tick(1000)
+  it('freezes the clock', () => {
+    cy.clock()
+    cy.mount(
+      <section className="status">
+        <Timer />
+      </section>,
+    )
+
+    cy.contains('.status__time', '00:00')
+    cy.wait(3000)
+    // the timer is still showing zeros
+    cy.contains('.status__time', '00:00')
   })
+
+  it('sets the clock to the given value', () => {
+    const now = moment()
+    cy.clock(now.clone().add(700, 'seconds').toDate())
+    cy.mount(
+      <SudokuContext.Provider value={{ timeGameStarted: now }}>
+        <section className="status">
+          <Timer />
+        </section>
+      </SudokuContext.Provider>,
+    )
+    // 700 seconds = 11 minutes and 40 seconds on the clock
+    cy.contains('.status__time', '11:40')
+  })
+
+  it('advances the clock using cy.tick')
 })
