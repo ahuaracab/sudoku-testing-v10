11b40456e9f773959eabbd94b3bd83baf4fdf750
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2022-06-13 19:14:44 -0400

    exclude all when doing run

--- cypress.config.js
+++ cypress.config.js
@@ -1,13 +1,24 @@
 const { defineConfig } = require('cypress')
 
+// cypress run
+// isInteractive: true,
+// isTextTerminal: true,
+// cypress open
+// isInteractive: true,
+// isTextTerminal: false,
+
 module.exports = defineConfig({
   projectId: '1qsjjk',
   e2e: {
     baseUrl: 'http://localhost:3000',
     setupNodeEvents(on, config) {
       // implement node event listeners here
+      if (config.isTextTerminal) {
+        return {
+          excludeSpecPattern: ['cypress/e2e/all.cy.js'],
+        }
+      }
     },
-    excludeSpecPattern: process.env.CI ? ['cypress/e2e/all.cy.js'] : [],
   },
 
   component: {
